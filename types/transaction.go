015c3776ed765dfe42b674f65fa9358c616bd453
Haowei Lee <haoweilee.tpe@gmail.com>
2024-05-11 15:04:17 +0800

    add transaction with UTXO model and grpc transport

---
+++ types/transaction.go
@@ -0,0 +1,38 @@
+package types
+
+import (
+	"crypto/sha256"
+
+	"github.com/leehaowei/blocker/crypto"
+	"github.com/leehaowei/blocker/proto"
+	pb "google.golang.org/protobuf/proto"
+)
+
+func SignTransaction(pk *crypto.PrivateKey, tx *proto.Transaction) *crypto.Signature {
+	return pk.Sing(HashTransaction(tx))
+}
+
+func HashTransaction(tx *proto.Transaction) []byte {
+	b, err := pb.Marshal(tx)
+	if err != nil {
+		panic(err)
+	}
+	hash := sha256.Sum256(b)
+	return hash[:]
+}
+
+func VerifyTransaction(tx *proto.Transaction) bool {
+	for _, input := range tx.Input {
+		var (
+			sig    = crypto.SignatureFromBytes(input.Signature)
+			pubKey = crypto.PublicKeyFromBytes(input.PublicKey)
+		)
+		// TODO: to fix: ensure we don't run into problem after verifaction
+		// because we've set the signature to nil
+		input.Signature = nil
+		if !sig.Verify(pubKey, HashTransaction(tx)) {
+			return false
+		}
+	}
+	return true
+}
---
+++ types/transaction_test.go
@@ -0,0 +1,52 @@
+package types
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/leehaowei/blocker/crypto"
+	"github.com/leehaowei/blocker/proto"
+	"github.com/leehaowei/blocker/util"
+	"github.com/stretchr/testify/assert"
+)
+
+// Test case
+// with balcance of 100 coins, send 5 coins to "AAA"
+// 2 outputs
+// 5 to the others
+// 95 back to our address
+func TestNewTransaction(t *testing.T) {
+	fromPrivKey := crypto.GeneratePrivateKey()
+	fromAddress := fromPrivKey.Public().Address().Byte()
+
+	toPrivKey := crypto.GeneratePrivateKey()
+	toAddress := toPrivKey.Public().Address().Byte()
+
+	input := &proto.TxInput{
+		PrevTxHash:   util.RandomHash(),
+		PrevOutIndex: 0,
+		PublicKey:    fromPrivKey.Public().Bytes(),
+	}
+
+	output1 := &proto.TxOutput{
+		Amount:  5,
+		Address: toAddress,
+	}
+	output2 := &proto.TxOutput{
+		Amount:  95,
+		Address: fromAddress,
+	}
+
+	tx := &proto.Transaction{
+		Version: 1,
+		Input:   []*proto.TxInput{input},
+		Outputs: []*proto.TxOutput{output1, output2},
+	}
+
+	sig := SignTransaction(fromPrivKey, tx)
+	input.Signature = sig.Bytes()
+
+	assert.True(t, VerifyTransaction(tx))
+
+	fmt.Printf("%+v\n", tx)
+}
